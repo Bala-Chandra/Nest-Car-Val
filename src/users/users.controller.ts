b8cb14ac205fb49bb3991af86d3df0e332814128
Bala chandra <bala.reddy.ece@gmail.com>
2026-01-05 08:47:43 +0530

    Customize Interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,35 @@
-import { UsersService } from './users.service';
 import {
   Body,
   Controller,
   Post,
   Get,
-  Param,
   Patch,
-  Query,
   Delete,
+  Param,
+  Query,
   NotFoundException,
   UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
-import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
+import { UsersService } from './users.service';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  @Post('signup')
-  async createUser(@Body() body: CreateUserDto) {
-    const user = await this.usersService.create(body.email, body.password);
-    return user;
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    this.usersService.create(body.email, body.password);
   }
 
-  @UseInterceptors(SerializeInterceptor)
+  //@UseInterceptors(new SerializeInterceptor(UserDto))
+  @Serialize(UserDto)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
-    console.log('Handler is running');
+    console.log('handler is running');
     const user = await this.usersService.findOne(parseInt(id));
     if (!user) {
       throw new NotFoundException('user not found');
@@ -36,19 +37,18 @@ export class UsersController {
     return user;
   }
 
-  // localhost:300/auth?email=asdf@asdf.com
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
   }
 
-  @Patch('/:id')
-  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
-    return this.usersService.update(parseInt(id), body);
-  }
-
   @Delete('/:id')
   removeUser(@Param('id') id: string) {
     return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 }
